01a674926097cb26b5c0cbd6565efe9f08dbce63
GitHub Action <action@github.com>
2021-03-03 09:44:26 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/jabaganz/topicdependencychecker/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.jabaganz.topicdependencychecker.services
+
+import com.github.jabaganz.topicdependencychecker.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
